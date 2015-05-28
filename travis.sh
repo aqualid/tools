a8cd00362fc3e8961f99d23899ffea023f97ead8
menify <menify@gmail.com>
2015-05-28 23:54:14 +0300

    CI fixes

--- travis.sh
+++ travis.sh
@@ -1,6 +1,8 @@
 set -ex
 
-coverage run --source=tools tests/run.py
+git clone --depth 1 https://github.com/aqualid/aqualid.git
+
+PYTHONPATH=$PWD/aqualid coverage run --source=tools tests/run.py
 
 flake8 --max-complexity=9 `find tools -name "[a-zA-Z]*.py"`
 flake8 `find tests -name "[a-zA-Z]*.py"`
